70ccefbc83c2cdbca4a9d94b08b8c712ff4871f7
Soulzerz <Soulzerz@github.com>
2020-01-29 17:20:35 -0400

    Added: versions, query, and plots file for self reference

---
+++ query.py
@@ -0,0 +1,29 @@
+from pandas import read_csv
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# load dataset
+url =   "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# shape
+print(dataset.shape)
+
+# head data
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+class_distribution = dataset.groupby('class').size()
+print(class_distribution)
+
+
---
+++ versions.py
@@ -0,0 +1,18 @@
+# Python version
+import sys
+print(f'Python: {sys.version}')
+# scipy
+import scipy
+print(f'scipy: {scipy.__version__}')
+# numpy
+import numpy
+print(f'numpy: {numpy.__version__}')
+# matplotlib
+import matplotlib
+print(f'matplotlib: {matplotlib.__version__}')
+# pandas
+import pandas
+print(f'pandas: {pandas.__version__}')
+# scikit-learn
+import sklearn
+print(f'sklearn: {sklearn.__version__}')
